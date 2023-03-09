7974d8f05dafe949b5bdbfa38301a30974ee230a
timeturnback <time.turnback@gmail.com>
2023-03-09 12:59:22 +0700

    test rollup again

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+import resolve from 'rollup-plugin-node-resolve';
+import filesize from 'rollup-plugin-filesize';
+import commonjs from 'rollup-plugin-commonjs';
+import cleanup from 'rollup-plugin-cleanup';
+import { uglify } from 'rollup-plugin-uglify';
+import json from 'rollup-plugin-json';
+
+const input = 'dist/cjs/index.js';
+
+const commonjsOptions = {
+  ignoreGlobal: true,
+  include: /node_modules/,
+  namedExports: {
+    '../../node_modules/prop-types/index.js': [
+      'elementType',
+      'bool',
+      'func',
+      'object',
+      'oneOfType',
+      'element'
+    ]
+  }
+};
+
+const plugins = [resolve(), commonjs(commonjsOptions), json(), cleanup(), uglify(), filesize()];
+
+const globals = {
+  react: 'React',
+  'react-doc': 'ReactDOM'
+};
+
+export default [
+  {
+    input,
+    output: {
+      file: 'dist/umd/redcloudsteam-ui.production.min.js',
+      format: 'umd',
+      name: 'RedcloudsteamUi',
+      globals
+    },
+    external: Object.keys(globals),
+    plugins
+  }
+];
